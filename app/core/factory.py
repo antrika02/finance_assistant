75d91c0b2a1cde6281769c8fa80369858c8bf983
Antrika Kashyap <antrikakashyap2@gmail.com>
2026-08-06 00:00:54 +0530

    chore: prepare project for production deployment

--- app/core/factory.py
+++ app/core/factory.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.router import api_router
 from app.core.exception_handlers import register_exception_handlers
 from app.core.logging import logger
 from app.core.settings import get_settings
-from fastapi.middleware.cors import CORSMiddleware
+
 
 def create_app() -> FastAPI:
     settings = get_settings()
@@ -15,20 +16,16 @@ def create_app() -> FastAPI:
         debug=settings.DEBUG,
     )
 
-    # Add CORS middleware
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
+        allow_origins=["*"],  # Restrict this in production
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
     )
 
-
-    # Register global exception handlers
     register_exception_handlers(app)
 
-    # Register all API routes
     app.include_router(api_router)
 
     logger.info(f"{settings.APP_NAME} started successfully.")
